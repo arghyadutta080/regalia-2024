830596330f500e54b617bf89454346c7bf52bbf9
ADLM10 <de.alimpan5@gmail.com>
2024-02-29 13:07:39 +0530

    initialised routes

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      <p className="font-annabel">Hello</p>
+      <p className="font-hollirood">Regalia</p>
+    </main>
+  )
+}
+
+export default Home
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <p className="font-annabel">Hello</p>
-      <p className="font-hollirood">Regalia</p>
-    </main>
-  );
-}
