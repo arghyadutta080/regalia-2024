3f151c69daa39397d424c8c2e3404e92afe55dc0
soumyarajbag <soumyarajbag@gmail.com>
2024-04-10 01:27:29 +0530

    branch change

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="text-3xl font-bold text-center text-regalia mt-20 min-h-[60vh]">
+    Only for Super Admins !
+  </div>
+
   )
 }
 
