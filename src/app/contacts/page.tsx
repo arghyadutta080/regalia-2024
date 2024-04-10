8998c50b08c2ff82b78601c9e3883157c79439a1
soumyarajbag <soumyarajbag@gmail.com>
2024-04-11 02:10:31 +0530

    fix:contacts

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,14 +1,14 @@
-import Heading from '@/components/common/Heading'
-import Block from '@/components/contacts/Block'
-import React from 'react'
+import Heading from "@/components/common/Heading";
+import Block from "@/components/contacts/Block";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='flex flex-col items-center mx-auto mt-10'>
-    <Heading text='Contact Us' />
-    <Block />
-  </div>
-  )
-}
+    <div className="mx-auto mt-10 flex flex-col items-center gap-5">
+      <Heading text="Contact Us" />
+      <Block />
+    </div>
+  );
+};
 
-export default page
+export default page;
