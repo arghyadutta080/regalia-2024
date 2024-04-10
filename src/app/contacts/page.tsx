f8d17a68c25e25949b3e6acc24e103058e34f921
soumyarajbag <soumyarajbag@gmail.com>
2024-04-11 01:57:42 +0530

    add:contact-page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,8 +1,13 @@
+import Heading from '@/components/common/Heading'
+import Block from '@/components/contacts/Block'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex flex-col items-center mx-auto mt-10'>
+    <Heading text='Contact Us' />
+    <Block />
+  </div>
   )
 }
 
