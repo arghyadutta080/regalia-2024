1fef05b2c5c612cd14af6c4fbda36ec2e0738bf6
Arghya <arghyadutta080@gmail.com>
2024-04-25 04:16:55 +0530

    add:gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,18 +1,22 @@
-import Link from 'next/link'
+import SparkleComponent from '@/components/ui/SparkleComponent';
+import { ParallaxScroll } from '@/components/ui/parallax-scroll';
+import { images } from '@/utils/constants/gallery';
 import React from 'react'
 
 const page = () => {
   return (
-    <div className="text-center text-[#c9a747] font-hollirood text-xl gap-2 md:text-3xl font-semibold items-center flex flex-col justify-center w-full mx-auto min-h-[80vh]">
-    <h1>Regalia 2024 is Loading !</h1>
-    <Link
-      href={"/"}
-      className="mt-10 border-2 border-[#c9a747] rounded-full hover:bg-[#c9a747] hover:text-white duration-300 text-[#c9a747] font-bold text-md md:text-xl px-5 py-1  md:px-10 md:py-2"
-    >
-      Go Home
-    </Link>
-  </div>
-  )
+    <div className="mx-auto max-w-full md:px-20">
+      <div className="mt-12 flex h-[8rem] w-full flex-col items-center justify-center overflow-hidden rounded-md bg-transparent">
+        <h1 className="relative z-20 text-center font-annabel text-4xl font-bold text-regalia md:text-3xl lg:text-5xl">
+          Gallery
+        </h1>
+        <div className="text-center">
+          <SparkleComponent />
+        </div>
+      </div>
+      <ParallaxScroll images={images} />
+    </div>
+  );
 }
 
 export default page
---
+++ src/utils/constants/gallery.ts
@@ -0,0 +1,14 @@
+export const images = [
+    "/assets/gallery/regalia2023_1.jpg",
+    "/assets/gallery/regalia2023_2.jpg",
+    "/assets/gallery/regalia2023_3.jpg",
+    "/assets/gallery/regalia2023_4.jpg",
+    "/assets/gallery/regalia2023_5.jpg",
+    "/assets/gallery/regalia2023_6.jpg",
+    "/assets/gallery/regalia2023_7.jpg",
+    "/assets/gallery/regalia2023_8.jpg",
+    "/assets/gallery/regalia2023_9.jpg",
+    "/assets/gallery/regalia2023_10.jpg",
+    "/assets/gallery/regalia2023_11.jpg",
+    "/assets/gallery/regalia2023_1.jpg",
+];
