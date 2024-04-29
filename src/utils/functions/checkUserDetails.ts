8a83e9bb27158816e085e56194349707714765bf
soumyarajbag <soumyarajbag@gmail.com>
2024-04-29 17:36:40 +0530

    fix

--- src/utils/functions/checkUserDetails.ts
+++ src/utils/functions/checkUserDetails.ts
@@ -1,9 +1,9 @@
 export const checkUserDetails = (data: Record<string, any>) => {
-    let isSatisfied = true;
-    Object.keys(data || {}).forEach((key) => {
-      if (data[key]==null || data[key] === "" || data[key] === undefined) {
-        isSatisfied = false;
-      }
-    });
-    return isSatisfied;
-  };
+  let isSatisfied = true;
+  Object.keys(data || {}).forEach((key) => {
+    if (key !== "college_roll" && (data[key] == null || data[key] === "" || data[key] === undefined)) {
+      isSatisfied = false;
+    }
+  });
+  return isSatisfied;
+};
