087aae0541c5899b45cde99eed06086f57b581cb
ADLM10 <de.alimpan5@gmail.com>
2024-06-29 02:50:37 +0530

    edited Hero.tsx and Events.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,8 +8,20 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center font-annabel">
       <div className="relative  flex w-full flex-row  gap-10   max-lg:items-center">
-        <Image src={"/assets/home/landing.png"} className="hidden xl:block" height={500} width={1000} alt="" />
-        <Image src={"/assets/home/mobile-hero.png"} className="block xl:hidden" height={500} width={1000} alt="" />
+        <Image
+          src={"/assets/home/landing.png"}
+          className="hidden xl:block"
+          height={500}
+          width={1000}
+          alt=""
+        />
+        <Image
+          src={"/assets/home/mobile-hero.png"}
+          className="block xl:hidden"
+          height={500}
+          width={1000}
+          alt=""
+        />
         <div className=" absolute -bottom-[4rem] hidden flex-col items-start gap-4 max-xl:mx-auto max-xl:w-full max-xl:text-center xl:bottom-60 xl:right-36 xl:flex">
           <FadeIn
             from="left"
@@ -26,7 +38,6 @@ const Hero = () => {
                   alt="rcc"
                 />
               </Link>
-             
             </div>
             <h1 className=" font-hollirood text-4xl">presents</h1>
           </FadeIn>
@@ -41,11 +52,19 @@ const Hero = () => {
                 2k24
               </div>
               <h1 className="text-4xl  text-white lg:text-9xl ">Regalia</h1>
-              <h1 className="font-hollirood mt-3 text-2xl w-full mx-auto">Inter College Annual Cultural fest</h1>
-              <h1 className="font-hollirood tracking-wider mt-3 text-2xl w-full mx-auto">7th & 8th May 2024 (Prelims)</h1>
-          <h1 className="font-hollirood mt-3 tracking-wider text-2xl w-full mx-auto">July 2024 (Finals)</h1>
+              <h1 className="mx-auto mt-3 w-full font-hollirood text-2xl">
+                Inter College Annual Cultural fest
+              </h1>
+              <h1 className="mx-auto mt-3 w-full font-hollirood text-2xl tracking-wider">
+                7th & 8th May 2024 (Prelims)
+              </h1>
+              <h1 className="mx-auto mt-3 w-full font-hollirood text-2xl tracking-wider">
+                10th & 11th July 2024 (Finals)
+              </h1>
+              <h1 className="mx-auto mt-3 w-full font-hollirood text-2xl tracking-wider">
+                Venue: Sarat Sadan , Howrah
+              </h1>
             </div>
-          
           </FadeIn>
         </div>
 
@@ -70,18 +89,26 @@ const Hero = () => {
             <h1 className=" text-xl">Presents</h1>
         </div>
       */}
-        <div className=" w-full mx-auto items-center jutify-center flex flex-col ">
+        <div className=" jutify-center mx-auto flex w-full flex-col items-center ">
           <div className="flex-row text-3xl md:text-5xl xl:flex xl:self-end xl:text-5xl">
             2k24
-          </div> 
+          </div>
           <h1 className="text-4xl text-white md:text-6xl lg:text-9xl ">
             Regalia
           </h1>
-          <h1 className="text-md text-regalia font-hollirood mt-2 w-full mx-auto md:text-2xl">Inter College Annual Cultural fest</h1>
-          <h1 className="tracking-wider mt-1 self-start flex md:text-xl lg:text-2xl">7th & 8th May 2024 (Prelims)</h1>
-          <h1 className="tracking-wider mt-1  flex md:text-xl lg:text-2xl text-center">July 2024 (Finals)</h1>
+          <h1 className="text-md mx-auto mt-2 w-full font-hollirood text-regalia md:text-2xl">
+            Inter College Annual Cultural fest
+          </h1>
+          <h1 className="mt-1 flex self-start tracking-wider md:text-xl lg:text-2xl">
+            7th & 8th May 2024 (Prelims)
+          </h1>
+          <h1 className="mt-1 flex  text-center tracking-wider md:text-xl lg:text-2xl">
+            10th & 11th July 2024 (Finals)
+          </h1>
+          <h1 className="mt-1 flex  text-center tracking-wider md:text-xl lg:text-2xl">
+            Venue: Sarat Sadan , Howrah
+          </h1>
         </div>
-      
       </div>
     </div>
   );
